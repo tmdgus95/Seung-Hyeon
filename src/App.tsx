757a19f80efc31a49748299f6a49c41f14171775
tmdgus95 <8571053@naver.com>
2023-03-07 21:59:24 +0900

    프로젝트 기본 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Portfolio from "./components/Portfolio";
+import Skills from "./components/Skills";
+import Visual from "./components/Visual";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <Visual />
+            <Skills />
+            <Portfolio />
+            <Contact />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Visual.tsx
@@ -0,0 +1,5 @@
+const Visual = () => {
+    return <div>Visual</div>;
+};
+
+export default Visual;
