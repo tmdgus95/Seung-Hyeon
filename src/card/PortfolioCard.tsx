bc79d2a6f072e5f1268fbb5254459f761244b932
tmdgus95 <8571053@naver.com>
2023-03-09 15:43:05 +0900

    포트폴리오 수정

---
+++ src/card/PortfolioCard.tsx
@@ -0,0 +1,69 @@
+import styled from "styled-components";
+
+const PortfolioCard = () => {
+    return (
+        <PortfolioCardWrap>
+            <PortfolioCardInner>
+                <h3>담화마켓</h3>
+                <PortfolioCardContent>
+                    <img
+                        src="/images/포트폴리오임시이미지.gif"
+                        alt="포트폴리오 이미지"
+                    />
+                    <div>
+                        <div>
+                            <p>주요기능</p>
+                            <p>술 검색기능,장바구니 기능,구매 기능,쿠폰 기능</p>
+                        </div>
+
+                        <div>
+                            <p>깃</p>
+                            <p>레파지토리 주소</p>
+                        </div>
+
+                        <div>
+                            <p>링크</p>
+                            <p>링크주소</p>
+                        </div>
+
+                        <div>
+                            <p>사용기술</p>
+                            <p>리액트 쿼리, 리액트 라우터 돔, Tailwind CSS</p>
+                        </div>
+                    </div>
+                </PortfolioCardContent>
+            </PortfolioCardInner>
+        </PortfolioCardWrap>
+    );
+};
+
+const PortfolioCardWrap = styled.div`
+    max-width: 1280px;
+    width: 1280px;
+`;
+const PortfolioCardInner = styled.div`
+    width: 100%;
+`;
+const PortfolioCardContent = styled.div`
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    img {
+        width: 40%;
+        max-height: 500px;
+    }
+    div {
+        width: 40%;
+        margin-left: 10px;
+    }
+    div > div {
+        width: 100%;
+        display: flex;
+    }
+    div > div > :nth-child(1) {
+        text-align: left;
+        width: 100px;
+    }
+`;
+
+export default PortfolioCard;
--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,5 +1,76 @@
+import styled from "styled-components";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper";
+import PortfolioCard from "../card/PortfolioCard";
+
 const Portfolio = () => {
-    return <div>Portfolio</div>;
+    return (
+        <PortfolioWrap>
+            <PortfolioInner>
+                <PortfolioContent>
+                    <h2>Portfolio</h2>
+                    <StyledPortfolioSwiper
+                        cssMode={true}
+                        navigation={true}
+                        pagination={{
+                            clickable: true,
+                        }}
+                        mousewheel={true}
+                        keyboard={true}
+                        slidesPerView={1}
+                        spaceBetween={30}
+                        loop={true}
+                        modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+                        className="mySwiper"
+                    >
+                        <StyledPortfolio>
+                            <PortfolioCard />
+                        </StyledPortfolio>
+                        <StyledPortfolio>
+                            <PortfolioCard />
+                        </StyledPortfolio>
+                        <StyledPortfolio>
+                            <PortfolioCard />
+                        </StyledPortfolio>
+                        <StyledPortfolio>
+                            <PortfolioCard />
+                        </StyledPortfolio>
+                        <StyledPortfolio>
+                            <PortfolioCard />
+                        </StyledPortfolio>
+                    </StyledPortfolioSwiper>
+                </PortfolioContent>
+            </PortfolioInner>
+        </PortfolioWrap>
+    );
 };
+
+const PortfolioWrap = styled.div``;
+const PortfolioInner = styled.div`
+    max-width: 1280px;
+    margin: 80px auto;
+`;
+const PortfolioContent = styled.div`
+    text-align: center;
+    height: 70vh;
+`;
+
+const StyledPortfolioSwiper = styled(Swiper)`
+    border-radius: 20px;
+    width: 100%;
+    height: 100%;
+`;
+
+const StyledPortfolio = styled(SwiperSlide)`
+    text-align: center;
+    font-size: 18px;
+    background: #fff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
 
 export default Portfolio;
