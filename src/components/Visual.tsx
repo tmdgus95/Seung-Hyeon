43de43d87bf598d4795a72e75c4789f5e5dcda61
tmdgus95 <8571053@naver.com>
2023-03-27 15:43:08 +0900

    비쥬얼 수정

--- src/components/Visual.tsx
+++ src/components/Visual.tsx
@@ -3,14 +3,18 @@ import styled from "styled-components";
 const Visual = () => {
     return (
         <VisualWrap id="Visual">
-            <h1>Seung Hyeon'S PORTFOLIO</h1>
-            <img src="/images/비쥬얼임시이미지.jpg" alt="메인사진" />
-            <h2>안녕하세요 Front-end developer 조승현입니다.</h2>
+            <img src="/images/비쥬얼이미지.jpg" alt="메인사진" />
+            <div>
+                <h1>Seung Hyeon'S PORTFOLIO</h1>
+                <h2>안녕하세요 Front-end developer 조승현입니다.</h2>
+            </div>
         </VisualWrap>
     );
 };
 
 const VisualWrap = styled.div`
+    display: flex;
+    justify-content: center;
     width: 100%;
     height: 600px;
     text-align: center;
@@ -18,18 +22,16 @@ const VisualWrap = styled.div`
     background-color: var(--color-visual-bg);
     padding: 75px 15px;
     color: var(--color-white);
-    h2 {
-        font-size: 26px;
+    h1 {
+        font-size: 36px;
         font-weight: 700;
     }
-    h3 {
-        font-size: 19px;
+    h2 {
+        font-size: 32px;
     }
     img {
-        margin: 20px 0;
-        width: 250px;
-        height: 250px;
-        border-radius: 100%;
+        width: 300px;
+        margin-right: 270px;
     }
 `;
 
